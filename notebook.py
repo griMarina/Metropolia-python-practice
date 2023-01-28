1ce27af2921d028507c887033299e8a2e1aaf14e
Marina Grigoreva <mgrigoreva12@gmail.com>
2023-01-28 10:55:25 +0200

    update Changing written file

--- notebook.py
+++ notebook.py
@@ -1,25 +1,43 @@
 # Chapter 5 Exercise 4: Notebook: Read and write to the notebook
+# Chapter 8 Exercise 4: Notebook: Changing the written file.
+
 import time
 
 
 def main():
+    filename = 'notebook.txt'
+    try:
+        open(filename, 'r')
+    except IOError:
+        open(filename, 'w')
+        print('No default notebook was found, created one.')
+
     while True:
-        print('(1) Read the notebook\n(2) Add note\n(3) Empty the notebook\n(4) Quit')
+        print('Now using file', filename,
+              '\n(1) Read the notebook\n(2) Add note\n(3) Empty the notebook\n(4) Change the notebook\n(5) Quit')
+
         selection = input('Please select one: ')
         if selection == '1':
-            with open('notebook.txt', 'r') as f:
+            with open(filename, 'r') as f:
                 content = f.read()
                 print(content)
         elif selection == '2':
-            with open('notebook.txt', 'a') as f:
+            with open(filename, 'a') as f:
                 note_time = time.strftime("%X %x")
                 new_note = input('Write a new note: ')
-                f.write(f'{new_note}:::{note_time}\n')
+                f.write(new_note + ':::' + note_time + '\n')
         elif selection == '3':
-            with open('notebook.txt', 'w') as f:
+            with open(filename, 'w') as f:
                 print('Notes deleted.')
                 f.close()
         elif selection == '4':
+            filename = input('Give the name of the new file: ')
+            try:
+                open(filename, 'r')
+            except IOError:
+                open(filename, 'w')
+                print('No notebook with that name detected, created one.')
+        elif selection == '5':
             print('Notebook shutting down, thank you.')
             break
         else:
